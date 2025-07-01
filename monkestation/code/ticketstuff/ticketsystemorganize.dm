c560c63f6540f739553d6482c9785b24ffd14b54
Siro <yamapeachsan@gmail.com>
2025-07-01 10:53:58 -0600

    mentorhelp basic tgui in place

--- monkestation/code/ticketstuff/ticketsystemorganize.dm
+++ monkestation/code/ticketstuff/ticketsystemorganize.dm
@@ -7,12 +7,13 @@
 /client/verb/mentorhelp2()
 	set category = "Mentor"
 	set name = "New Mentor Help"
+	GLOB.mentor_help_ui_handler.ui_interact(mob)
+	to_chat(src, span_boldnotice("Mentorhelp failing to open or work? <a href='byond://?src=[REF(src)];tguiless_mentorhelp=1'>Click here</a>"))
 
-	if(usr?.client?.prefs.muted & MUTE_ADMINHELP)
-		to_chat(src,
-			type = MESSAGE_TYPE_MODCHAT,
-			html = "<span class='danger'>Error: MentorPM: You are muted from Mentorhelps. (muted).</span>",
-			confidential = TRUE)
-		return
-	//GLOB.mentor_help_ui_handler.ui_interact(mob)
-	//to_chat(src, span_boldnotice("Adminhelp failing to open or work? <a href='byond://?src=[REF(src)];tguiless_adminhelp=1'>Click here</a>"))
+/client/verb/no_tgui_mentorhelp(message as message)
+	set name = "NoTguiMentorhelp"
+	set hidden = TRUE
+
+	message = trim(message)
+
+	GLOB.mentor_help_ui_handler.perform_mentorhelp(src, message)
---
+++ tgui/packages/tgui/interfaces/Mentorhelp.tsx
@@ -0,0 +1,42 @@
+import { useBackend, useLocalState } from '../backend';
+import { TextArea, Stack, Button } from '../components';
+import { Window } from '../layouts';
+
+export const Mentorhelp = (props) => {
+  const { act } = useBackend();
+  const [mhelpMessage, setMhelpMessage] = useLocalState('mhelp_message', '');
+  return (
+    <Window title="Create Mentorhelp" theme="admin" height={300} width={500}>
+      <Window.Content
+        style={{
+          'background-image': 'none',
+        }}
+      >
+        <Stack vertical fill>
+          <Stack.Item grow>
+            <TextArea
+              autoFocus
+              height="100%"
+              value={{ mhelpMessage }}
+              placeholder="Mentor help"
+              onChange={(e, value) => setMhelpMessage(value)}
+            />
+          </Stack.Item>
+          <Stack.Item>
+            <Button
+              color="good"
+              fluid
+              content="Submit"
+              textAlign="center"
+              onClick={() =>
+                act('mhelp', {
+                  message: mhelpMessage,
+                })
+              }
+            />
+          </Stack.Item>
+        </Stack>
+      </Window.Content>
+    </Window>
+  );
+};
